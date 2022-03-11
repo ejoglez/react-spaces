2c39791f485c792803267b4bca6348116935388c
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-03-11 10:58:48 -0500

    Our blog Posts

--- my-react-space/src/components/Blog/BlogItem.js
+++ my-react-space/src/components/Blog/BlogItem.js
@@ -5,7 +5,10 @@ function BlogItem({blog,favBlog, retire}){
     return (
         <div onClick={()=>favBlog(blog)} className="blogPost">
             <h3>{blog.title}</h3>
-            <p>{blog.body}</p>
+            <p>{blog.body} </p>
+            <a href={blog.link}>{blog.link}</a>
+            <br></br>
+            <br></br>
             <button className="purge" onClick={()=>retire(blog)} >Purge</button>
         </div>
     )
